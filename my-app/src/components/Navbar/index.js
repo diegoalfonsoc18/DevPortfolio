1209602d3125886e5685f6c7f46f02c787a253d8
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-09 12:29:43 -0500

    Add navBar

---
+++ my-app/src/components/Navbar/index.js
@@ -0,0 +1,19 @@
+import "./components.css";
+import dev from "../../icon/dev.png";
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="logo">
+          <img src={dev} alt="Dev"></img>
+        </div>
+        <div className="textNav">
+          <p className="name">DIEGO</p>
+          <p className="lastname">ALFONSO</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

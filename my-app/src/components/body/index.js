dc1e9f9f9f2cd871e4f6a47fd4a40e3825a6efc6
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-10 19:56:20 -0500

    fix general

--- my-app/src/components/body/index.js
+++ my-app/src/components/body/index.js
@@ -6,48 +6,50 @@ import discord from "../../icon/discord.png";
 const Body = () => {
   return (
     <div className="body">
-      <div className="wrapperMain">
-        <div className="menu"></div>
-        <div className="wrapper">
-          <div className="imageCircle">
-            <div className="circle"></div>
+      <div className="bodyWrapper">
+        <div className="wrapperMain">
+          <div className="menu"></div>
+          <div className="wrapper">
+            <div className="imageCircle">
+              <div className="circle"></div>
+            </div>
+          </div>
+          <div className="menu">
+            <div className="wrapperMenu">
+              <a href="#about">ABOUT</a>
+              <a href="#projects">PROJECTS</a>
+              <a href="#contact">CONTACT</a>
+            </div>
           </div>
         </div>
-        <div className="menu">
-          <div className="wrapperMenu">
-            <a href="#about">ABOUT</a>
-            <a href="#projects">PROJECTS</a>
-            <a href="#contact">CONTACT</a>
-          </div>
-        </div>
-      </div>
-      <div className="footerMain">
-        <div className="footerBody">
-          <div className="title">
-            <h3>Web Developer</h3>
-          </div>
-          <div className="socialIcon">
-            <a
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/diego-alfonso-castillo/"
-              target="_blank"
-            >
-              <img src={linkedin} alt="linkendin"></img>
-            </a>
-            <a
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/diego-alfonso-castillo/"
-              target="_blank"
-            >
-              <img src={github} alt="github icon"></img>
-            </a>
-            <a
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/diego-alfonso-castillo/"
-              target="_blank"
-            >
-              <img src={discord} alt="icon discord"></img>
-            </a>
+        <div className="footerMain">
+          <div className="footerBody">
+            <div className="title">
+              <h3>Web Developer</h3>
+            </div>
+            <div className="socialIcon">
+              <a
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/diego-alfonso-castillo/"
+                target="_blank"
+              >
+                <img src={linkedin} alt="linkendin"></img>
+              </a>
+              <a
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/diego-alfonso-castillo/"
+                target="_blank"
+              >
+                <img src={github} alt="github icon"></img>
+              </a>
+              <a
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/diego-alfonso-castillo/"
+                target="_blank"
+              >
+                <img src={discord} alt="icon discord"></img>
+              </a>
+            </div>
           </div>
         </div>
       </div>
